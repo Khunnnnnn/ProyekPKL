1cdc4e5d9c020833cd953cde382d5f699b052e72
Khunnnnnn <anggahadi447@gmail.com>
2022-02-15 14:05:03 +0800

    Merapikan Tampilan

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
-<<<<<<< HEAD
 use App\Http\Controllers\KelasController;
-=======
 use App\Http\Controllers\AdminController;
->>>>>>> 6f81caa012f58d4caa855eb133f703f55fdd0a8a
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KuisController;
 
@@ -31,17 +28,11 @@ Route::get('/', function () {
     return view('layouts.dashboard');
 });
 
-<<<<<<< HEAD
-Route::resource('/kelas', KelasController::class);
-
 Route::get('/laporan', function () {
     return view('layouts.laporan');
 });
-=======
-Route::get('/kelas', function () {
-    return view('layouts.kelas');
-});
+
 
 Route::resource('/kuis', KuisController::class);
 Route::resource('/admin-quiz', AdminController::class);
->>>>>>> 6f81caa012f58d4caa855eb133f703f55fdd0a8a
+Route::resource('/kelas', KelasController::class);
